551de46418519254ac847dbaca662852da57390d
skrungly <kingsleymcdonald@outlook.com>
2025-04-29 06:35:19 +0100

    feat: improved program structure, allow player query by name

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,8 @@ import os
 
 import pymysql
 from flask import Flask
-from flask_restful import Api
 
 app = Flask(__name__)
-api = Api(app)
 
 db = pymysql.connect(
     host=os.environ.get("MYSQL_HOST", "mysql"),
@@ -15,7 +13,4 @@ db = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
 )
 
-from app.resources import PlayerAPI, PlayerListAPI  # noqa: E402
-
-api.add_resource(PlayerAPI, "/players/<int:user_id>", endpoint="player")
-api.add_resource(PlayerListAPI, "/players", endpoint="players")
+from app import api
---
+++ app/api.py
@@ -0,0 +1,60 @@
+from flask import abort
+from flask_restful import Api, Resource
+
+from app import app, db
+
+api = Api(app)
+
+
+class PlayerAPI(Resource):
+    def get(self, user_id):
+        with db.cursor() as cursor:
+            cursor.execute(
+                "SELECT id, name, safe_name, priv, country, creation_time, "
+                "  latest_activity, preferred_mode, userpage_content "
+                "FROM users WHERE id=(%s)",
+                (user_id,)
+            )
+
+            player_data = cursor.fetchone()
+            if not player_data:
+                abort(404)
+
+            player_data["stats"] = {}
+
+            cursor.execute("SELECT * FROM stats WHERE id=(%s)", user_id)
+            for stats in cursor.fetchall():
+                player_data["stats"][stats["mode"]] = stats
+
+        return player_data
+
+    def put(self, user_id):
+        ...
+
+
+class PlayerNameAPI(PlayerAPI):
+    @staticmethod
+    def _id_from_name(username):
+        with db.cursor() as cursor:
+            cursor.execute("SELECT id FROM users WHERE name=(%s)", username)
+            user_id = cursor.fetchone()
+            if not user_id:
+                abort(404)
+
+        return user_id["id"]
+
+    def get(self, username):
+        return super().get(self._id_from_name(username))
+
+    def put(self, username):
+        return super().put(self._id_from_name(username))
+
+
+class PlayerListAPI(Resource):
+    def get(self):
+        ...
+
+
+api.add_resource(PlayerNameAPI, "/players/<username>", endpoint="player_by_name")
+api.add_resource(PlayerAPI, "/players/<int:user_id>", endpoint="player_by_id")
+api.add_resource(PlayerListAPI, "/players", endpoint="players")
