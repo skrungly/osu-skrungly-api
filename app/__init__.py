bdb9412cd6534da5b4ab4732a52cf81d0f75e05b
skrungly <kingsleymcdonald@outlook.com>
2025-05-20 19:06:22 +0100

    feat: enable deployment with docker compose

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 import os
 
+import dotenv
 import pymysql
 from flask import Flask
 from flask_restx import Api
+
+dotenv.load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
 
 db = pymysql.connect(
-    host=os.environ.get("MYSQL_HOST", "mysql"),
+    host=os.environ["MYSQL_HOST"],
+    port=int(os.environ["MYSQL_PORT"]),
     user=os.environ["MYSQL_USER"],
     password=os.environ["MYSQL_PASSWORD"],
     db=os.environ["MYSQL_DATABASE"],
