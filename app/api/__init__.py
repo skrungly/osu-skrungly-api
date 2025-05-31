e7733f6b95d87d347c44af5b504dd9ac2d5924f0
skrungly <kingsleymcdonald@outlook.com>
2025-05-31 10:45:16 +0100

    fix: get api docs to show up again

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,25 @@
-from flask_restx import Api
+from flask import render_template
+from flask_restx import Api, apidoc
 
 from app import app
 
-api = Api(app)
+
+@apidoc.apidoc.add_app_template_global
+def swagger_static(filename):
+    return f"./swaggerui/{filename}"
+
+
+api = Api(app, version="0.1", title="osu!skrungly API")
+
+
+@api.documentation
+def swagger_docs():
+    return render_template(
+        "swagger-ui.html",
+        title=api.title,
+        specs_url="./swagger.json"
+    )
+
 
 from app.api import (  # noqa: F401 E402
     auth, beatmaps, players, scores, stats
