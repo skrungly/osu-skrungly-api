964281f54b5937b8b77ce80b2715d553321f96ce
kingsley mcdonald <kingsleymcdonald@outlook.com>
2025-10-12 10:52:58 +0100

    test: use serialised data to avoid hard-coding strings

--- tests/endpoints/test_maps_by_id.py
+++ tests/endpoints/test_maps_by_id.py
@@ -4,7 +4,7 @@ import pytest
 def test_get_ranked_bancho_beatmap(client, expected_data):
     expected_map = expected_data("beatmap")
 
-    response = client.get("/maps/811675")
+    response = client.get(f"/maps/{expected_map['id']}")
 
     assert response.status_code == 200
 
@@ -13,13 +13,15 @@ def test_get_ranked_bancho_beatmap(client, expected_data):
 
 
 # there is no map with id 2 on bancho, so it should 404
-@pytest.mark.parametrize("map_id", ["", "/2", "/aaaaa"])
+@pytest.mark.parametrize("map_id", ["2", "aaaaa"])
 def test_get_inexistent_bancho_beatmap(client, map_id):
-    response = client.get(f"/maps{map_id}")
+    response = client.get(f"/maps/{map_id}")
 
     assert response.status_code == 404
 
 
-def test_bad_methods_beatmaps(client):
+def test_bad_methods_beatmaps(client, expected_data):
+    map_id = expected_data("beatmap")["id"]
+
     for http_method in (client.post, client.put, client.delete):
-        assert http_method("/maps/811675").status_code == 405
+        assert http_method(f"/maps/{map_id}").status_code == 405
--- tests/endpoints/test_mapsets_by_id.py
+++ tests/endpoints/test_mapsets_by_id.py
@@ -4,7 +4,7 @@ import pytest
 def test_get_ranked_bancho_mapset(client, expected_data):
     expected_map = expected_data("beatmap")
 
-    response = client.get("/mapsets/370340")
+    response = client.get(f"/mapsets/{expected_map['set_id']}")
 
     assert response.status_code == 200
     assert len(response.json) == 5  # mapset has 5 difficulties
@@ -13,13 +13,15 @@ def test_get_ranked_bancho_mapset(client, expected_data):
         assert all(key in map_data for key in expected_map)
 
 
-@pytest.mark.parametrize("set_id", ["", "/2", "/aaaaa"])
+@pytest.mark.parametrize("set_id", ["2", "aaaaa"])
 def test_get_inexistent_bancho_mapset(client, set_id):
-    response = client.get(f"/mapsets{set_id}")
+    response = client.get(f"/mapsets/{set_id}")
 
     assert response.status_code == 404
 
 
-def test_bad_methods_mapsets(client):
+def test_bad_methods_mapsets(client, expected_data):
+    set_id = expected_data("beatmap")["set_id"]
+
     for http_method in (client.post, client.put, client.delete):
-        assert http_method("/mapsets/370340").status_code == 405
+        assert http_method(f"/mapsets/{set_id}").status_code == 405
